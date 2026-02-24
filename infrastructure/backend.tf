e67fef549ba442e268c38598bec6798e4ad91642
Axel <axel.ramone@gmail.com>
2026-02-24 16:07:15 -0600

    chore: complete terraform setup (#3)
    See 7b4f3f92eaefcc26eba49d451ec88bbcd14eb6bf for reason why this was
    necessary. This file was mistakenly omitted from PR #2

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,10 +1,11 @@
 resource "google_storage_bucket" "backend" {
-  name = "billing-notifier-tf-state-${var.environment}"
+  name = "billing-notifier-${var.environment}-tf-state"
   project = var.google_project_id
   location = "US"
   storage_class = "STANDARD"
   force_destroy = false
   public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
 
   versioning {
     enabled = true
