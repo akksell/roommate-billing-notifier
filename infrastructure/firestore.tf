b1e50dc5b8e4c3355fd71b5a41d598fb43872793
Axel Ramone <axel.ramone@gmail.com>
2026-02-28 16:50:09 -0600

    chore: provision firestore db
    This firestore db will hold the most recent history id from messages in
    the pubsub topic as well as the roommates and upcoming bills that are
    due. Not a multi region db due to cost and scope of the project.

--- infrastructure/firestore.tf
+++ infrastructure/firestore.tf
@@ -1,8 +1,8 @@
-/*
-resource "google_firestore_database" "database" {
+resource "google_firestore_database" "default" {
   project     = var.google_project_id
-  name        = "(default)"
-  location_id = var.firestore_location_id
+  name        = "billing"
+  location_id = var.google_project_region
   type        = "FIRESTORE_NATIVE"
+  database_edition = "STANDARD"
+  delete_protection_state = "DELETE_PROTECTION_ENABLED"
 }
-*/
